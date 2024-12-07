57854d0952747731fe36e7c67f2d3bc3800297e8
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2024-12-07 22:47:49 +0100

    Added a image to the screen

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -3,15 +3,18 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class gamePanel extends JPanel {
+public class gamePanel extends JPanel implements Runnable{
     //SCREEN SETTINGS
-    final int originalTitleSize = 16; //16x16 time
+    final int originalTileSize = 16; //16x16 time
     final int scale= 3;
-    final int titleSize=originalTitleSize*scale; //48x48 title
+
+    final int tileSize=originalTileSize*scale; //48x48 title
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = titleSize * maxScreenCol; // 768 pixels
-    final int screenHeight = titleSize * maxScreenRow; // 576 pixels
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
 
     public gamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -19,5 +22,43 @@ public class gamePanel extends JPanel {
         this.setDoubleBuffered(true);
     }
 
+    public void startGameThread(){
+        gameThread=new Thread(this);
+        gameThread.start();
+    }
 
+
+    @Override
+    public void run() {
+
+        while(gameThread!=null){
+
+            //System.out.println("Game loop is running");
+
+
+            //1. UPDATE: Updating information such as character information
+            update();
+
+            //2. DRAW: Drawing the screen with the updated information
+            repaint();
+        }
+
+
+    }
+
+    public void update(){
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100,100,tileSize,tileSize);
+
+        g2.dispose();
+    }
 }
